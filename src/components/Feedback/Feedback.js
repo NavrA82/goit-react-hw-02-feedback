0da789c0c82b5ead4a5a4ce027e845ff6e3d718f
NavrA82 <124286244+NavrA82@users.noreply.github.com>
2023-08-11 23:16:06 +0300

    task-1.2

--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -1,5 +1,9 @@
 // import PropTypes from 'prop-types';
-import { FeedbackStats } from './FeedbackStyled';
+import {
+  FeedbackStats,
+  FeedbackButtonWrapper,
+  FeedbackButton,
+} from './FeedbackStyled';
 import { Component } from 'react';
 
 export class Feedback extends Component {
@@ -8,6 +12,7 @@ export class Feedback extends Component {
     neutral: 0,
     bad: 0,
   };
+
   handleOnClickBtnGood = () => {
     this.setState(prevState => {
       return {
@@ -29,20 +34,40 @@ export class Feedback extends Component {
       };
     });
   };
+  countTotalFeedback = () => {
+    const sum = this.state.good + this.state.neutral + this.state.bad;
+    return sum;
+  };
+
+  countPositiveFeedbackPercentage() {
+    const percentage = Math.round(
+      (this.state.good * 100) /
+        (this.state.good + this.state.neutral + this.state.bad)
+    );
+    return percentage;
+  }
 
   render() {
     return (
       <FeedbackStats>
         <h2>Please leave feedback</h2>
-        <div>
-          <button onClick={this.handleOnClickBtnGood}>Good</button>
-          <button onClick={this.handleOnClickBtnNeutral}>Neutral</button>
-          <button onClick={this.handleOnClickBtnBad}>Bad</button>
-        </div>
+        <FeedbackButtonWrapper>
+          <FeedbackButton onClick={this.handleOnClickBtnGood}>
+            Good
+          </FeedbackButton>
+          <FeedbackButton onClick={this.handleOnClickBtnNeutral}>
+            Neutral
+          </FeedbackButton>
+          <FeedbackButton onClick={this.handleOnClickBtnBad}>
+            Bad
+          </FeedbackButton>
+        </FeedbackButtonWrapper>
         <h2>Statistics</h2>
         <p>Good:{this.state.good}</p>
         <p>Neutral:{this.state.neutral}</p>
         <p>Bad:{this.state.bad}</p>
+        <p>Total:{this.countTotalFeedback()}</p>
+        <p>Positive feedback: {this.countPositiveFeedbackPercentage()}%</p>
       </FeedbackStats>
     );
   }
--- src/components/Feedback/FeedbackStyled.js
+++ src/components/Feedback/FeedbackStyled.js
@@ -7,3 +7,17 @@ export const FeedbackStats = styled.div`
   margin: 40px auto;
   max-width: 360px;
 `;
+export const FeedbackButtonWrapper = styled.div`
+  display: flex;
+  gap: 8px;
+`;
+export const FeedbackButton = styled.button`
+  border: 1px solid blue;
+  border-radius: 5px;
+  padding: 4px 8px;
+  &:hover {
+    background-color: blue;
+    color: white;
+    cursor: pointer;
+  }
+`;
